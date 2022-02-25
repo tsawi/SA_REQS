156b37a920a666c31e5f244b039059fefc3af9bf
Theresa Sawi <theresa@Theresas-Air.lan>
2022-02-25 18:36:00 -0500

    finding preprocessing

---
+++ notebooks/src/specufex_processing/1_preprocessing/1_makeWaveformsDataset.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 17:00:19 2021
+example: Parkfield repeaters::
+@author: theresasawi
+"""
+
+
+import argparse
+import glob
+import os
+
+import h5py
+import numpy as np
+import obspy
+import pandas as pd
+import yaml
+
+from functions import dataframe2hdf
+from functions.generators import gen_wf_from_folder, load_wf
+
+parser = argparse.ArgumentParser()
+parser.add_argument("config_filename", help="Path to configuration file.")
+args = parser.parse_args()
+
+# load config file
+with open(args.config_filename, 'r') as stream:
+    try:
+        config = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+# pull out config values for conciseness
+path_config = config["paths"]
+key = path_config["key"]
+data_config = config['dataParams']
+station = data_config["station"]
+channel = data_config["channel"]
+channel_ID = data_config["channel_ID"]
+sampling_rate = data_config["sampling_rate"]
+
+# build path strings
+dataH5_name = f'data_{key}.h5'
+projectPath = path_config["projectPath"]
+pathWF = path_config["pathWF"]
+dataH5_path = projectPath + 'data/H5files/' + dataH5_name
+wf_cat_out_path = projectPath + 'wf_cat_out.csv'
+
+if not os.path.isdir(projectPath + '/H5files/'):
+    os.mkdir(projectPath + '/H5files/')
+
+# get global catalog
+cat = pd.read_csv(path_config["pathCat"])
+
+# get list of waveforms and sort
+wf_filelist = [pathWF+x for x in cat["filename"]]
+wf_filelist.sort()
+print(wf_filelist[0])
+
+if data_config["filetype"] == '.txt':
+    wf_test = np.loadtxt(wf_filelist[0])
+    lenData = len(wf_test)
+
+else:
+    wf_test = obspy.read(wf_filelist[0])
+    lenData = len(wf_test[0].data)
+
+# define generator (function)
+gen_wf = gen_wf_from_folder(wf_filelist,
+                            lenData,
+                            data_config["channel_ID"])
+
+
+# clear old H5 if it exists, or else error will appear
+if os.path.exists(dataH5_path):
+    os.remove(dataH5_path)
+
+# add catalog and waveforms to H5
+evID_keep = [] #list of wfs to keep
+
+with h5py.File(dataH5_path,'a') as h5file:
+    global_catalog_group =  h5file.create_group("catalog/global_catalog")
+    dataframe2hdf(cat, global_catalog_group)
+
+    waveforms_group  = h5file.create_group("waveforms")
+    station_group = h5file.create_group(f"waveforms/{station}")
+    channel_group = h5file.create_group(f"waveforms/{station}/{channel}")
+
+    dupl_evID = 0 #duplicate event IDs?? not here, sister
+
+    evID_keep = []
+    for n, ev in cat.iterrows():
+        if n%500==0:
+            print(n, '/', len(cat))
+        data = load_wf(pathWF+ev["filename"], lenData, channel_ID)
+        if data is not None:
+            channel_group.create_dataset(name=str(ev["ev_ID"]), data=data)
+            evID_keep.append(ev["ev_ID"])
+        else:
+            print(ev.ev_ID, " not saved")
+
+    processing_group = h5file.create_group(f"{station}/processing_info")
+    processing_group.create_dataset(name= "sampling_rate_Hz", data=sampling_rate)#,dtype='S')
+    # processing_group.create_dataset(name= "station_info", data=station_info)
+    # processing_group.create_dataset(name= "calibration", data=calib)#,dtype='S')
+    # processing_group.create_dataset(name= "orig_formata", data=_format)#,dtype='S')
+    # processing_group.create_dataset(name= "instr_response", data=instr_response,dtype='S')
+    processing_group.create_dataset(name= "lenData", data=lenData)#,dtype='S')
+    print("processing_group")
+
+print(dupl_evID, ' duplicate events found and avoided')
+print(n + 1 - dupl_evID, ' waveforms loaded')
+
+# save final working catalog to csv
+cat_keep_wf = cat[cat['ev_ID'].isin(evID_keep)]
+
+if os.path.exists(wf_cat_out_path):
+    os.remove(wf_cat_out_path)
+
+cat_keep_wf.to_csv(wf_cat_out_path, index=False)
+
+print(len(cat_keep_wf), ' events in wf catalog')
---
+++ notebooks/src/specufex_processing/1_preprocessing/3_runSpecUFEx.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 19 12:02:51 2021
+
+@author: theresasawi
+"""
+
+import argparse
+
+import h5py
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import yaml
+
+from specufex import BayesianNonparametricNMF, BayesianHMM
+
+# command line argument instead of hard coding to config file
+parser = argparse.ArgumentParser()
+parser.add_argument("config_filename", help="Path to configuration file.")
+args = parser.parse_args()
+
+with open(args.config_filename, 'r') as stream:
+    try:
+        config = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print("F you!", exc)
+
+variable_list = []
+value_list = []
+
+# pull out config values for conciseness
+path_config = config["paths"]
+key = path_config["key"]
+
+data_config = config['dataParams']
+station = data_config["station"]
+channel = data_config["channel"]
+channel_ID = data_config["channel_ID"]
+sampling_rate = data_config["sampling_rate"]
+
+sgram_config = config["sgramParams"]
+nfft = sgram_config["nfft"]
+fmin, fmax = sgram_config["fmin"], sgram_config["fmax"]
+
+# build path strings
+dataH5_name = f'data_{key}.h5'
+projectPath = path_config["projectPath"]
+pathWF = path_config["pathWF"]
+
+dataH5_name =  'data_' + path_config["h5name"] #f'data_{key}.hdf5'
+dataH5_path = projectPath + 'H5files/' + dataH5_name
+SpecUFEx_H5_name = 'SpecUFEx_' + path_config["h5name"] #f'SpecUFEx_{key}.hdf5'
+SpecUFEx_H5_path = projectPath + 'H5files/' + SpecUFEx_H5_name
+pathWf_cat  = projectPath + 'wf_cat_out.csv'
+pathSgram_cat = projectPath + f'sgram_cat_out_{key}.csv'
+sgramMatOut = projectPath + 'matSgrams/'## for testing
+
+sgram_cat = pd.read_csv(pathSgram_cat)
+
+#%% get spectrograms from H5
+
+# a NUMPY WAY (can certainly be cleaned/tightened up-- very verbose as written:)
+# and I got the shape wrong.. the list .append() adds to axis=0, not 2.
+
+# with h5py.File(SpecUFEx_H5_path,'a') as fileLoad:
+#     count = 0
+#     for evID in fileLoad['spectrograms']:
+#         count += 1
+#         if count==1:
+#             specMat = fileLoad['spectrograms'].get(evID)[:]
+#             sg_shp = np.shape(specMat)
+#
+#
+# X = np.empty((sg_shp[0],sg_shp[1],count))
+#
+# with h5py.File(SpecUFEx_H5_path,'a') as fileLoad:
+#     i_count = 0
+#     for evID in fileLoad['spectrograms']:
+#         specMat = fileLoad['spectrograms'].get(evID)[:]
+#         Xis = specMat
+#         if np.shape(Xis) != sg_shp:
+#             print(evID, i_count,': SHAPE IS DIFFERENT!')
+#
+#         X[:,:,i_count] = Xis
+#         i_count += 1
+#         if i_count%500 == 0:
+#             print(i_count)
+
+# ================
+# LIST WAY (how it was written before):
+X = []
+
+with h5py.File(SpecUFEx_H5_path,'a') as fileLoad:
+    for evID in fileLoad['spectrograms']:
+        specMat = fileLoad['spectrograms'].get(evID)[:]
+        X.append(specMat)
+
+    X = np.array(X)
+
+# ================
+print(np.shape(X))
+print(X[:,:,-1])
+
+
+
+#%% ============================================================
+# Running SpecUFEx
+#%% ============================================================
+
+specparams = config["specufexParams"]
+
+print('Running NMF')
+nmf = BayesianNonparametricNMF(X.shape)
+for i in range(specparams["nmf_nbatch"]):
+    # pick random sample
+    print(f"Batch {i}")
+    sample = np.random.choice(X.shape[0], specparams["nmf_batchsz"])
+    nmf.fit(X[sample], verbose=1)
+
+Vs = nmf.transform(X)
+# print how long it took
+
+#%%
+print('Running HMM')
+hmm = BayesianHMM(nmf.num_pat, nmf.gain)
+for i in range(specparams["hmm_nbatch"]):
+    print(f"Batch {i}")
+    sample = np.random.choice(Vs.shape[0], specparams["nmf_batchsz"])
+    hmm.fit(Vs)
+
+fingerprints, As, gams = hmm.transform(Vs)
+
+print(fingerprints[0])
+
+# show a fingerprint if you want to .. but not useful for running remotely..
+#plt.imshow(fingerprints[0])
+#plt.show()
+#%%
+#%%
+#%%
+
+# =============================================================================
+# save output to H5
+# =============================================================================
+print('writing all output to h5')
+with h5py.File(SpecUFEx_H5_path,'a') as fileLoad:
+
+
+    ##fingerprints are top folder
+    if 'fingerprints' in fileLoad.keys():
+        del fileLoad["fingerprints"]
+    fp_group = fileLoad.create_group('fingerprints')
+
+    if 'SpecUFEX_output' in fileLoad.keys():
+        del fileLoad["SpecUFEX_output"]
+    out_group = fileLoad.create_group("SpecUFEX_output")
+
+    # write fingerprints: ===============================
+    for i, evID in enumerate(fileLoad['spectrograms']):
+        fp_group.create_dataset(name= evID, data=fingerprints[i])
+        #ACM_group.create_dataset(name=evID,data=As[i]) #ACM
+        #STM_group.create_dataset(name=evID,data=gam[i]) #STM
+
+    # write the SpecUFEx out: ===========================
+    # maybe include these, but they are not yet tested.
+    #ACM_group = fileLoad.create_group("SpecUFEX_output/ACM")
+    #STM_group = fileLoad.create_group("SpecUFEX_output/STM")
+
+    # for i, evID in enumerate(fileLoad['spectrograms']):
+    #     ACM_group.create_dataset(name=evID,data=As[i]) #ACM
+    #     STM_group.create_dataset(name=evID,data=gam[i]) #STM
+
+    gain_group = fileLoad.create_group("SpecUFEX_output/ACM_gain")
+    W_group                      = fileLoad.create_group("SpecUFEX_output/W")
+    EB_group                     = fileLoad.create_group("SpecUFEX_output/EB")
+    ## # # delete probably ! gain_group                   = fileLoad.create_group("SpecUFEX_output/gain")
+    #RMM_group                    = fileLoad.create_group("SpecUFEX_output/RMM")
+
+    W_group.create_dataset(name='W',data=nmf.EW)
+    EB_group.create_dataset(name=evID,data=hmm.EB)
+    gain_group.create_dataset(name='gain',data=nmf.gain) #same for all data
+    # RMM_group.create_dataset(name=evID,data=RMM)
